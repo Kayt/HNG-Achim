776e2955498e6f3f1e90c81d5ec0af4e69517214
Achim Munene <achim_munene@hotmail.com>
2018-05-03 00:50:56 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>HNG Stage 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link href="https://fonts.googleapis.com/css?family=Rosarivo|Sarpanch" rel="stylesheet">
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="welcome">
+                <h1 class="welcometo">Welcome To</h1>
+                <h3 class="hng"><span id="hng">HNG</span> INTERNSHIP 4</h3>
+            </div>
+        
+            <div class="today">
+                <div class="top">
+                    <div class="wednesday"><?php echo date("l"); ?></div>
+                </div>
+                
+                <div class="bottom">
+                    <div class="two"><?php echo date("d"); ?></div>
+        
+                    <div class="five"><?php echo date("m"); ?></div>
+            
+                    <div class="year"><?php echo date("y"); ?></div>
+                </div>
+                
+            </div>
+    </div>
+    
+</body>
+</html>
